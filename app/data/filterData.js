4f3b44ed479ad0cb0bf5980958b3d2c72737ee56
HarDhillon <har7173@gmail.com>
2024-05-16 14:45:45 +0100

    Create data file for structure and remove select component
    - data folder now contains table data
    - Create the structure of the filter data
    
    - select is not needed as we will create a multi-select

---
+++ app/data/filterData.js
@@ -0,0 +1,14 @@
+export const filters = {
+    "Strategy": ['Thematic', 'Factors', 'Equity Income', 'Capital Strength', 'Currency Hedge', 'ESG'],
+    "AssetClass": {
+        "Market": ['Developed', 'Emerging'],
+        "Region": {
+            "Asia Pacific": [],
+            "Europe": ["Eurozone", "Germany", "Switzerland", "United Kingdom"],
+            "Global": [],
+            "North America": ["United States"]
+        }
+    },
+    "Market & Region": ['Developed', 'Emerging'],
+    "Style": ['Index', 'Active']
+};
